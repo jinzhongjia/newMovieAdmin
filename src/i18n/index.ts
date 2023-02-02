619218df31382560504860538d276f69f0844639
影翼 <jinzhongjia@qq.com>
2023-02-02 17:10:01 +0800

    [进度]:增加i18n配置，但i18n功能是否增加仍在考虑中
    1

---
+++ src/i18n/index.ts
@@ -0,0 +1,8 @@
+import { createI18n } from "vue-i18n";
+const i18n = createI18n({
+	// something vue-i18n options here ...
+});
+
+//TODO
+
+export default i18n;
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import router from "./router/index";
 import "./style.css";
 import App from "./App.vue";
+import I18n from "./i18n/index";
 
 const app = createApp(App);
 app.use(router);
+app.use(I18n);
+
 app.mount("#app");
