3d2b3b7affff454a9db2e4439fe54a6e6482e99f
影翼 <jinzhongjia@qq.com>
2023-02-02 17:04:43 +0800

    [进度]:登陆页完成，多端适配完成

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,6 +1,57 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const account = ref("");
+const password = ref("");
+const remember = ref(false);
+</script>
 <template>
-  <n-card title="卡片"> 卡片内容 </n-card>
+	<div class="login-box">
+		<n-card class="login" title="登陆">
+			<div class="login-input">
+				<n-input
+					v-model:value="account"
+					type="text"
+					placeholder="请输入用户名"
+				/>
+			</div>
+
+			<div class="login-input">
+				<n-input
+					v-model:value="password"
+					type="password"
+					show-password-on="click"
+					placeholder="请输入密码"
+				/>
+			</div>
+			<div class="login-input">
+				<n-checkbox v-model:checked="remember">记住密码</n-checkbox>
+			</div>
+			<div class="login-input">
+				<n-button class="login-button" type="primary"> 登陆 </n-button>
+			</div>
+		</n-card>
+	</div>
 </template>
 
-<style></style>
+<style>
+.login-box {
+	width: 100vw;
+	height: 100vh;
+}
+.login {
+	max-width: 100vw;
+	max-height: 100vh;
+	width: 400px;
+	/* height: 200px; */
+	/* margin: auto; */
+	position: relative;
+	top: 50%;
+	left: 50%;
+	transform: translateX(-50%) translateY(-50%);
+}
+.login-input {
+	margin-bottom: 15px;
+}
+.login-button {
+	width: 100%;
+}
+</style>
